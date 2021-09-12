2745984953a00708e28fb3909b51040ecd5e5db3
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-09-12 19:31:48 +0530

    added installation script

---
+++ install.sh
@@ -0,0 +1,27 @@
+mkdir -p ~/.config/
+mkdir -p ~/.local/bin/
+mkdir -p ~/.local/share/music
+mkdir -p ~/Pictures/background ~/Pictures/Screenshot
+touch ~/.local/share/music/directories
+ln -s ~/git/rdconfig/.config/X11 ~/.config/X11
+ln -s ~/git/rdconfig/.config/alacritty ~/.config/alacritty
+ln -s ~/git/rdconfig/.config/awesome ~/.config/awesome
+ln -s ~/git/rdconfig/.config/fontconfig ~/.config/fontconfig
+ln -s ~/git/rdconfig/.config/lxc ~/.config/lxc
+ln -s ~/git/rdconfig/.config/mpd ~/.config/mpd
+ln -s ~/git/rdconfig/.config/mpv ~/.config/mpv
+ln -s ~/git/rdconfig/.config/mutt ~/.config/mutt
+ln -s ~/git/rdconfig/.config/ncmpcpp ~/.config/ncmpcpp
+ln -s ~/git/rdconfig/.config/nvim ~/.config/nvim
+ln -s ~/git/rdconfig/.config/ranger ~/.config/ranger
+ln -s ~/git/rdconfig/.config/sxiv ~/.config/sxiv
+ln -s ~/git/rdconfig/.config/tmux ~/.config/tmux
+ln -s ~/git/rdconfig/.config/zathura ~/.config/zathura
+ln -s ~/git/rdconfig/.config/zsh ~/.config/zsh
+ln -s ~/git/rdconfig/.config/compton.conf ~/.config/compton
+ln -s ~/git/rdconfig/.config/emoji ~/.config/emoji
+ln -s ~/git/rdconfig/.config/mimeapps.list ~/.config/mimeapps.list
+ln -s ~/git/rdconfig/.local/bin/scripts ~/.local/bin/scripts
+ln -s ~/git/rdconfig/Templates ~/Templates
+ln -s ~/git/rdconfig/.profile ~/.profile
+ln -s ~/git/rdconfig/.zprofile ~/.zprofile
