f51c0ac27e68b2229eafddd4627c57bfc7a36546
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-09-12 22:57:16 +0530

    add_playlist.sh -> add_playlist

--- .local/bin/scripts/add_playlist.sh
+++
@@ -1,8 +0,0 @@
-name=$(echo "" | dmenu -p "Enter name: ")
-if [ -z $name ]; then
-  echo "Playlist name empty"
-else
-  touch ~/.config/mpd/playlists/$name.m3u
-  echo $HOME/Music/$name >> ~/.local/share/music/directories
-  mkdir -p "~/Music/$name"
-fi
