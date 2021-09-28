caf97d5e8fa9c06af52efbbd852ed80b509270ea
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-09-29 00:35:09 +0530

    added git-signs to workflow

--- .config/nvim/lua/rd/plugins.lua
+++ .config/nvim/lua/rd/plugins.lua
@@ -65,7 +65,7 @@ return require("packer").startup {
     use { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
     use { "junegunn/fzf", run = "./install --all" }
     use "junegunn/fzf.vim"
-    -- use "lewis6991/gitsigns.nvim"
+    use "lewis6991/gitsigns.nvim"
 
     colorscheme_plugins(use)
   end,
