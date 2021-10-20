35bcbb8fbe2c55b3ea232dcce9ca078eda8d04b9
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-10-20 18:06:14 +0530

    updated nvim-notify max_timeout

--- .config/nvim/plugin/notify.lua
+++ .config/nvim/plugin/notify.lua
@@ -1,1 +1,5 @@
-vim.notify = require("notify")
+vim.notify = function(msg, log_level, opts)
+  opts = opts or {}
+  opts.timeout = opts.timeout or 2000
+  require('notify')(msg, log_level, opts)
+end
