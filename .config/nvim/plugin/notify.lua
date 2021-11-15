510fdcdcf928477e3b39efcfeeb21436379b401c
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-11-15 17:32:23 +0530

    I don't like floating notifications TBH

--- .config/nvim/plugin/notify.lua
+++ .config/nvim/plugin/notify.lua
@@ -1,5 +1,5 @@
-vim.notify = function(msg, log_level, opts)
-  opts = opts or {}
-  opts.timeout = opts.timeout or 2000
-  require "notify"(msg, log_level, opts)
-end
+-- vim.notify = function(msg, log_level, opts)
+--   opts = opts or {}
+--   opts.timeout = opts.timeout or 2000
+--   require "notify"(msg, log_level, opts)
+-- end
