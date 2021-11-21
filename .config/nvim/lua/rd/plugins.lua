4b86ff5dfeac619c0952a467ffcd072d7919c0eb
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-11-21 20:47:04 +0530

    some tpope stuff

--- .config/nvim/lua/rd/plugins.lua
+++ .config/nvim/lua/rd/plugins.lua
@@ -86,7 +86,8 @@ return require("packer").startup {
     use "p00f/godbolt.nvim"
     use "lukas-reineke/indent-blankline.nvim"
     use "saadparwaiz1/cmp_luasnip"
-
+    use "tpope/vim-vinegar"
+    use "tpope/vim-eunuch"
     colorscheme_plugins(use)
 
     -- Plugins that I am not using but maybe useful!
