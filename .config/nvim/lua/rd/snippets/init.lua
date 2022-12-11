281d8607e043eb05153561403e81ab3429b1fc3f
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2022-12-11 19:18:03 +0530

    range query for idempotent

--- .config/nvim/lua/rd/snippets/init.lua
+++ .config/nvim/lua/rd/snippets/init.lua
@@ -1,11 +1,14 @@
 vim.cmd [[
-  imap <silent><expr> <c-j> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<c-k>'
-  inoremap <silent> <c-k> <cmd>lua require('luasnip').jump(-1)<CR>
+imap <silent><expr> <C-j> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
+" -1 for jumping backwards.
+inoremap <silent> <C-k> <cmd>lua require'luasnip'.jump(-1)<Cr>
 
-  imap <silent><expr> <C-l> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-l>'
+snoremap <silent> <C-j> <cmd>lua require('luasnip').jump(1)<Cr>
+snoremap <silent> <C-k> <cmd>lua require('luasnip').jump(-1)<Cr>
 
-  snoremap <silent> <c-j> <cmd>lua require('luasnip').jump(1)<CR>
-  snoremap <silent> <c-k> <cmd>lua require('luasnip').jump(-1)<CR>
+" For changing choices in choiceNodes (not strictly necessary for a basic setup).
+imap <silent><expr> <C-l> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+smap <silent><expr> <C-l> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
 ]]
 
 require "rd.snippets.all"
