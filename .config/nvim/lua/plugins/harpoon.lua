66b5f90c9fd86276d3567b6117f4ff8d7ecbfc63
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-06-29 13:22:12 +0530

    harpoon setup for lua

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -5,8 +5,19 @@ return {
     local harpoon = require "harpoon"
     harpoon:setup {
       term = {
-        select = function(n)
-          P(n)
+        buffers = {},
+        select = function(n, buffers)
+          local bufnr = buffers[n] or -1
+          local is_valid_buf = vim.api.nvim_buf_is_valid(bufnr)
+          if not is_valid_buf then
+            bufnr = vim.api.nvim_create_buf(false, false)
+          end
+          local win = vim.api.nvim_get_current_win()
+          vim.api.nvim_win_set_buf(win, bufnr)
+          if not is_valid_buf then
+            vim.fn.termopen(vim.o.shell)
+          end
+          buffers[n] = bufnr
         end,
       },
     }
@@ -23,7 +34,7 @@ return {
         harpoon:list():select(i)
       end)
       vim.keymap.set("n", string.format("<space>s%s", key), function()
-        harpoon.config.term.select(i)
+        harpoon.config.term.select(i, harpoon.config.term.buffers)
       end)
     end
   end,
