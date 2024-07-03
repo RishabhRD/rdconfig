29811b5cd5f9fa9ebf0f4117b2cf74115b650409
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-07-03 22:20:30 +0530

    dap settings

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -42,6 +42,44 @@ return {
           disconnect = "⏻",
         },
       },
+      layouts = {
+        {
+          elements = {
+            {
+              id = "scopes",
+              size = 0.25,
+            },
+            {
+              id = "breakpoints",
+              size = 0.25,
+            },
+            {
+              id = "stacks",
+              size = 0.25,
+            },
+            {
+              id = "watches",
+              size = 0.25,
+            },
+          },
+          position = "left",
+          size = 80,
+        },
+        {
+          elements = {
+            {
+              id = "repl",
+              size = 0.5,
+            },
+            {
+              id = "console",
+              size = 0.5,
+            },
+          },
+          position = "bottom",
+          size = 10,
+        },
+      },
     }
 
     require("dapui").setup(ui_config)
