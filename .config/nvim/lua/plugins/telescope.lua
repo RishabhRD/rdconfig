79223f34ca16bb4f808bd991c73962a8a40ecd80
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-12-07 16:47:35 +0530

    telescope history doesn't work

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,10 +4,10 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-      "nvim-telescope/telescope-smart-history.nvim",
       "nvim-telescope/telescope-ui-select.nvim",
       "ryanoasis/vim-devicons",
       "kyazdani42/nvim-web-devicons",
+      "kkharji/sqlite.lua",
     },
     config = function()
       require "rd.telescope.setup"
--- .config/nvim/lua/rd/telescope/setup.lua
+++ .config/nvim/lua/rd/telescope/setup.lua
@@ -1,5 +1,4 @@
 local actions = require "telescope.actions"
-local data = assert(vim.fn.stdpath "data") --[[@as string]]
 
 require("telescope").setup {
   defaults = {
@@ -27,10 +26,6 @@ require("telescope").setup {
         override_file_sorter = true, -- override the file sorter
         case_mode = "smart_case", -- or "ignore_case" or "respect_case"
         -- the default case_mode is "smart_case"
-      },
-      history = {
-        path = vim.fs.joinpath(data, "telescope_history.sqlite3"),
-        limit = 100,
       },
       ["ui-select"] = {
         require("telescope.themes").get_dropdown {},
