ffa0b5952ce74b72d7fa88506d82f5e46a3d8cb0
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-12-23 12:56:49 +0530

    neotest config

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -21,7 +21,14 @@ return {
       vim.keymap.set("n", "<leader>to", function()
         neotest.output_panel.toggle()
       end)
+      local summary_open = false
       vim.keymap.set("n", "<leader>ts", function()
+        if summary_open then
+          summary_open = false
+        else
+          neotest.run.run(vim.fn.getcwd())
+          summary_open = true
+        end
         neotest.summary.toggle()
       end)
     end,
