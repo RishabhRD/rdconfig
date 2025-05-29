128d1117435d3f746b000986fa5010fd183289ce
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-05-30 03:53:50 +0530

    update the plugin

--- .config/nvim/lua/rd/autocmd.lua
+++ .config/nvim/lua/rd/autocmd.lua
@@ -28,6 +28,7 @@ local function setup()
     pattern = "swift",
     callback = function()
       vim.opt_local.cindent = true
+      vim.opt_local.smartindent = true
       vim.opt_local.shiftwidth = 2
       vim.opt_local.softtabstop = 2
       vim.opt_local.expandtab = true
