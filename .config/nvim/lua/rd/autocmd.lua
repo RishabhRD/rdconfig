b319b6db3866eebc8cc302730bfa71686b1771e3
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-05-30 02:01:16 +0530

    better swift support for vim

--- .config/nvim/lua/rd/autocmd.lua
+++ .config/nvim/lua/rd/autocmd.lua
@@ -22,6 +22,31 @@ local function setup()
     end,
   })
   local format = require("rd.autoformat").format
+
+  -- Swift support is bad with treesitter.
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "swift",
+    callback = function()
+      vim.opt_local.cindent = true
+      vim.opt_local.shiftwidth = 2
+      vim.opt_local.softtabstop = 2
+      vim.opt_local.expandtab = true
+
+      -- Enable continuation of comments when pressing Enter or o/O
+      vim.opt_local.formatoptions:append "r"
+      vim.opt_local.formatoptions:append "o"
+
+      -- Set comments for:
+      -- s1:/*   - start of block comment
+      -- mb:*    - middle of block comment
+      -- ex:*/   - end of block comment
+      -- ://     - line comment
+      -- :///    - doc line comment (triple slash)
+      -- s2:/**  - start of doc block comment
+      vim.opt_local.comments = "s1:/*,mb:*,ex:*/,:///,://,s2:/**"
+    end,
+  })
+
   local lsp = require("rd.lsp").executeLspPreWriteHooks
   local bufWritePreTasks = {
     format,
