f37d53b5086d112a2188a521b91de62a769e1259
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-08-07 23:12:58 +0530

    better transparency settings

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -31,6 +31,49 @@ return {
   "NTBBloodbath/sweetie.nvim",
   "gruvbox-community/gruvbox",
   {
+    "xiyaowong/transparent.nvim",
+    config = function()
+      -- Optional, you don't have to run setup.
+      require("transparent").setup {
+        -- table: default groups
+        groups = {
+          "Normal",
+          "NormalNC",
+          "Comment",
+          "Constant",
+          "Special",
+          "Identifier",
+          "Statement",
+          "PreProc",
+          "Type",
+          "Underlined",
+          "Todo",
+          "String",
+          "Function",
+          "Conditional",
+          "Repeat",
+          "Operator",
+          "Structure",
+          "LineNr",
+          "NonText",
+          "SignColumn",
+          "CursorLine",
+          "CursorLineNr",
+          "StatusLine",
+          "StatusLineNC",
+          "EndOfBuffer",
+        },
+        -- table: additional groups that should be cleared
+        extra_groups = {},
+        -- table: groups you don't want to clear
+        exclude_groups = { "CursorLine" },
+        -- function: code to be executed after highlight groups are cleared
+        -- Also the user event "TransparentClear" will be triggered
+        on_clear = function() end,
+      }
+    end,
+  },
+  {
     "maxmx03/fluoromachine.nvim",
     -- config = function()
     --   local fm = require "fluoromachine"
--- .config/nvim/lua/rd/colors.lua
+++ .config/nvim/lua/rd/colors.lua
@@ -1,5 +1,3 @@
-local transparent = false
-
 local function set_custom_highlight()
   vim.cmd [[hi Cursor guifg=white guibg=black]]
   vim.cmd [[hi DiagnosticError guifg=#db4b4b]]
@@ -10,53 +8,22 @@ end
 
 local function colorscheme(str)
   vim.cmd(string.format("colorscheme %s", str))
-  if transparent == true then
-    vim.cmd [[hi Normal guibg=NONE ctermbg=NONE]]
-  end
   set_custom_highlight()
-end
-
-local function toggle_transparency()
-  if transparent == false then
-    vim.cmd [[hi Normal guibg=NONE ctermbg=NONE]]
-    vim.cmd [[hi SignColumn guibg=none]]
-    vim.cmd [[hi CursorLineNR guibg=None]]
-    transparent = true
-  else
-    vim.o.background = "dark"
-    transparent = false
-  end
-  set_custom_highlight()
-end
-
-local function make_transparent()
-  vim.cmd [[hi Normal guibg=NONE ctermbg=NONE]]
-  vim.cmd [[hi SignColumn guibg=none]]
-  vim.cmd [[hi CursorLineNR guibg=None]]
-  transparent = true
-end
-
-local function make_opaque()
-  vim.o.background = "dark"
-  transparent = false
 end
 
 local function my_default_config()
   colorscheme "tokyonight-night"
-  vim.cmd [[hi MatchParen guibg=#373737 guifg=#00000]]
+  -- vim.cmd [[hi MatchParen guibg=#373737 guifg=#00000]]
 end
 
 local function setup()
   my_default_config()
-  vim.keymap.set("n", "<leader>tr", toggle_transparency)
+  vim.keymap.set("n", "<leader>tr", ":TransparentToggle<CR>")
   vim.keymap.set("n", "<leader>td", my_default_config)
 end
 
 return {
-  toggle_transparency = toggle_transparency,
   colorscheme = colorscheme,
-  make_transparent = make_transparent,
-  make_opaque = make_opaque,
   my_default_config = my_default_config,
   setup = setup,
 }
