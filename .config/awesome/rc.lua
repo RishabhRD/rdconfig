637cb6d47ee7f2aa7299304f8897c351d099f49b
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2026-08-15 14:05:41 +0530

    only required configs

--- .config/awesome/rc.lua
+++
@@ -1,8 +0,0 @@
-pcall(require, "luarocks.loader")
-require("global").setup()
-require("error").setup()
-require("keybinds").setup()
-require("mouse").setup()
-require("rules").setup()
-require("signal").setup()
-require("apps").setup()
